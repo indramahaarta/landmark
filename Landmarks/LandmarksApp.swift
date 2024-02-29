4567ed15ba0a4e58ababe8079f0bcf6e4973e766
indramhaarta <indramhrt@gmail.com>
2024-02-29 20:28:36 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by I Made Indra Mahaarta on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
